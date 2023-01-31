703a87c308ffe8d257e5b1d3d090c4ae1571b0dd
Yuanjie Wu <yuanjiewu925@gmail.com>
2023-01-31 23:51:42 +1300

    create add page for adding person add attributes to person model item

--- LabWeb.CoreBusiness/PersonModel.cs
+++ LabWeb.CoreBusiness/PersonModel.cs
@@ -1,14 +1,21 @@
-﻿namespace LabWeb.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabWeb.CoreBusiness
 {
     public class PersonModel
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; } = string.Empty;
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
         public string ResearchArea { get; set; } = string.Empty;
+        [Required, EmailAddress]
         public string EmailAddress { get; set; } = string.Empty;
         public string Biography { get; set; } = string.Empty;
+        [Required]
         public string ImageURL { get; set; } = string.Empty;
         public string WebLink { get; set; } = string.Empty;
     }
